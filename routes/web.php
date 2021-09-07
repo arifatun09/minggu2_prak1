5558199dd6d80886ebbfa9e218ba761bc1a95452
arifatun09 <arifatunnisa.090501@gmail.com>
2021-09-07 12:40:02 +0700

    praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Selamat Datang";
+});
+Route::get('/about', function () {
+    echo "NIM: 2031710118 <br>";
+    echo "Nama: Arifatun Nisa";
+});
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman Artikel dengan ID " . $id;
 });
